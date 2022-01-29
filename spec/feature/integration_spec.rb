fa89b7b9876091355c228709fc62c78181a0a5ff
isaacy13 <iyastros@gmail.com>
2022-01-29 13:07:37 -0600

    added test cases

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -10,3 +10,37 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content('harry potter')
   end
 end
+
+
+RSpec.describe 'Creating an author', type: :feature do
+  scenario 'valid inputs' do
+    visit new_author_path
+    fill_in 'Title', with: 'j.k. rowling'
+    click_on 'Create Author'
+    visit authors_path
+    expect(page).to have_content('j.k. rowling')
+  end
+end
+
+
+RSpec.describe 'Creating a price', type: :feature do
+  scenario 'valid inputs' do
+    visit new_price_path
+    fill_in 'price', with: 50
+    click_on 'Create Price'
+    visit prices_path
+    expect(page).to have_content(50)
+  end
+end
+
+RSpec.describe 'Creating an date', type: :feature do
+  time = Time.new
+
+  scenario 'valid inputs' do
+    visit new_date_path
+    fill_in 'Title', with: time
+    click_on 'Create Date'
+    visit authors_path
+    expect(page).to have_content(time)
+  end
+end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -15,3 +15,45 @@ RSpec.describe Book, type: :model do
     expect(subject).not_to be_valid
   end
 end
+
+
+RSpec.describe Author, type: :model do
+  author = Author.new(title: 'j.k. rowling')
+
+  it 'is valid with valid attributes' do
+    expect(author).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    author.title = nil
+    expect(author).not_to be_valid
+  end
+end
+
+
+RSpec.describe Price, type: :model do
+  price = Price.new(price: 50)
+
+  it 'is valid with valid attributes' do
+    expect(price).to be_valid
+  end
+
+  it 'is not valid when nil' do
+    price.price = nil
+    expect(price).not_to be_valid
+  end
+end
+
+
+RSpec.describe Date, type: :model do
+  date = Date.new(date: Time.new)
+
+  it 'is valid with valid attributes' do
+    expect(date).to be_valid
+  end
+
+  it 'is not valid when nil' do
+    date.date = nil
+    expect(date).not_to be_valid
+  end
+end
